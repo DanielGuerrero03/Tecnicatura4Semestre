2772eecb8983bcfe2fdb0fef894050bc5ef4d24d
Daniel Guerrero <50740422+DanielGuerrero03@users.noreply.github.com>
2023-08-31 20:00:45 -0300

    E-commercer 2023 clase 02

--- e_commerce 2023/client/js/index.js
+++ e_commerce 2023/client/js/index.js
@@ -1,4 +1,5 @@
 const shopContent = document.getElementById('shopContent');
+const cart = [];
 
 productos.forEach((producto) => {
     const content = document.createElement('div');
@@ -7,5 +8,22 @@ productos.forEach((producto) => {
     <h3>${producto.productName}</h3>
     <p>$${producto.price}</p>
     `;
-    shopContent.appendChild(content);
+    shopContent.append(content);
+
+    const buyButton = document.createElement('button');
+    buyButton.innerText = 'Comprar';
+    
+    content.append(buyButton);
+
+    buyButton.addEventListener('click', () => {
+        cart.push({
+            id: producto.id,
+            productName: producto.productName,
+            price: producto.price,
+            quanty: producto.quanty,
+            img: producto.img,
+
+        })
+        console.log(cart);
+});
 });
