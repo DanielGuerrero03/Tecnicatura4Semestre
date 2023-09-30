d11acf27fb7b57faa53df4575e3bdd6caa7e619b
Daniel Guerrero <50740422+DanielGuerrero03@users.noreply.github.com>
2023-09-29 21:41:31 -0300

    Clase 06 Python video 3

--- Python_4/Clase02/dar_formato_str.py
+++ Python_4/Clase02/dar_formato_str.py
@@ -8,4 +8,13 @@ mensaje_con_formato = "Hola, mi nombre es %s y tengo %d años" %(nombre, edad)
 # Creamos una tupla
 persona = ("Carla", "Gonzalez", 5000.00)
 mensaje_con_formato = "Hola %s %s . Tu sueldo es $ %.2f" # %persona # Aqui le pasamos el objeto que es una tupla
-print(mensaje_con_formato %persona)
+#print(mensaje_con_formato %persona)
+
+nombre = "Juan"
+edad = 19
+sueldo = 3000
+mensaje_con_formato = "Nombre {} Edad {} Sueldo {:.2f}".format(nombre, edad, sueldo)
+print(mensaje_con_formato)
+
+mensaje = "Nombre {0} Edad {1} Sueldo {2:.2f}".format(nombre, edad, sueldo)
+print(mensaje)
