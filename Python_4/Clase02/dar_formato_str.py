383b7f527e82b95ba2d5640005cc82bdcc0c222c
Daniel Guerrero <50740422+DanielGuerrero03@users.noreply.github.com>
2023-09-29 21:37:16 -0300

    Clase 06 Python
    seguimos con mas cosas sobre dar formato a un string

--- Python_4/Clase02/dar_formato_str.py
+++ Python_4/Clase02/dar_formato_str.py
@@ -4,4 +4,8 @@
 nombre = "Daniel"
 edad = 32
 mensaje_con_formato = "Hola, mi nombre es %s y tengo %d años" %(nombre, edad)
-print(mensaje_con_formato)
+
+# Creamos una tupla
+persona = ("Carla", "Gonzalez", 5000.00)
+mensaje_con_formato = "Hola %s %s . Tu sueldo es $ %.2f" # %persona # Aqui le pasamos el objeto que es una tupla
+print(mensaje_con_formato %persona)
