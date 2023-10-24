412000697619368834ba392d5b05c38ec5089eea
Daniel Guerrero <50740422+DanielGuerrero03@users.noreply.github.com>
2023-10-24 17:57:56 -0300

    Clase 8 video 2 java

--- Java_4/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java_4/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,7 +1,13 @@
 package utn.tienda_libros;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.vista.LibroFrom;
+
+import java.awt.EventQueue;
 
 @SpringBootApplication
 public class TiendaLibrosApplication {
@@ -9,5 +15,15 @@ public class TiendaLibrosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaLibrosApplication.class, args);
 	}
+		ConfigurableApplicationContext contextoSring =
+				new SpringApplicationBuilder(TiendaLibrosApplication.class)
+						.headless(false)
+						.web(WebApplicationType.NONE)
+						.run(args);
 
+	//Ejecutamos el codigo para cargar el formulario
+	EventQueue.invokeLater(() -> {
+		//Obetenemos el objeto form a traves del spring
+		LibroFrom libroFrom = contextoSring.getBean(LibroFrom.class);
+	})
 }
--- Java_4/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Java_4/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -1,13 +1,17 @@
 package utn.tienda_libros.vista;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
+import java.awt.*;
 
+@Component
 public class LibroFrom extends JFrame {
     LibroServicio libroServicio;
     private JPanel panel;
+
 
     @Autowired
     public LibroFrom(LibroServicio libroServicio) {
@@ -20,6 +24,11 @@ public class LibroFrom extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setSize(900, 700);
-
+        //Para obtener las dimensiones de la ventana
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension tamanioPantalla = toolkit.getScreenSize();
+        int x = (tamanioPantalla.width - getWidth()/ 2) ;
+        int y = (tamanioPantalla.height - getHeight()/ 2) ;
+        setLocation(x, y);
     }
 }
